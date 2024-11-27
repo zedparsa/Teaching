b007a472a744f2421e2a48c802fac366ace5f46a
Parsa Karimian <161445341+zedparsa@users.noreply.github.com>
2024-11-27 19:24:18 +0330

    Create 1-Operators.C

---
+++ C/Episode 3/1-Operators.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+    //Arithmetic operators
+    // ( + , - , * , / , % )
+
+    //Eaquality / Relational operators
+    // ( == , != , > , < , >= , <= )
+
+    //Logical / Conditional operators
+    // ( ! , && , || )
+
+    //Bitwise operators
+    // ( & , | , ^ , << , >> , ~ )
+
+    //Assignment operators
+    // ( = , += , -= , *= , /= , %= )
+
+    return 0;
+}
