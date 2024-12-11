ff757dcd0cfde598d49d0cc279dec48ab27e9685
Parsa Karimian <161445341+zedparsa@users.noreply.github.com>
2024-12-11 20:25:43 +0330

    Create 3. Strings Tips .C

---
+++ C/Episode 6/3. Strings Tips .C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() 
+{
+    //char txt1[] = "We are the so-called "Vikings" from the north.";
+    char txt2[] = "We are the so-called \"Vikings\" from the north.";
+
+    printf("%s\n", txt2);
+
+    return 0;
+}
+
+/*
+
+\' --->	'	Single quote
+\" --->	"	Double quote
+\\ --->	\	Backslash 
+\n ------>	New Line	
+\t ------>  Tab	
+\0 ------>	Null
+
+*/
