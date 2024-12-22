d912690ad615e995451e7a8e51f71b8f11d1223b
Parsa Karimian <161445341+zedparsa@users.noreply.github.com>
2024-12-22 21:26:25 +0330

    Update Functions part 1 .C

--- C/Episode 7/Functions part 1 .C
+++ C/Episode 7/Functions part 1 .C
@@ -7,14 +7,35 @@ returnType functionName(parameters) {
 }
 */
 void hello(); //prototype
+void printNumber(int);
+int square(int num);
+float getPI();
 
 int main()
 {
     hello(); //call hello function
+    printNumber(5); //call printNumber function with 5 as argument
+    int square_result = square(6); //call square function with 6 as argument
+    printf("Square of 6: %d\n", square_result);
+    float pi = getPI();
+    printf("Value of PI: %.2f\n", pi);
 
+    
     return 0;
 }
 
 void hello() {
     printf("Hello Friend !\n");
+}
+
+void printNumber(int num) {
+    printf("Number: %d\n", num);
+}
+
+int square(int num) {
+    return num * num;
+}
+
+float getPI() {
+    return 3.14;
 }
